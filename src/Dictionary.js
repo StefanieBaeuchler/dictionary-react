fb776231e691123410b2b8e6b27b731b94671258
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2022-12-27 11:47:19 +0100

    Integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse (response){
+    console.log(response.data[0]);
+
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
     alert(`${keyword}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function updateKeyword(event) {
