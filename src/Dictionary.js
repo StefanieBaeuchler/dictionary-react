2fc6e5b55f9883ad64c31b8a162afefaaee1b3c3
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2022-12-28 09:57:31 +0100

    Rendered the meaning section

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
+  const [results, setResults] = useState(null);
 
   function handleResponse (response){
-    console.log(response.data[0]);
+    setResults(response.data[0]);
 
   }
 
@@ -27,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={handleSubmit}>
         <input type="search" onChange={updateKeyword} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
