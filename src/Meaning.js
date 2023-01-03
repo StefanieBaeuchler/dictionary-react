b2a7180ee6b2b4f535d3379186430b19917a52f0
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2023-01-03 11:47:10 +0100

    Added phonetic section

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,21 +3,20 @@ import Synonyms from "./Synonyms";
 import Examples from "./Examples";
 
 export default function Meaning(props) {
-  console.log(props.meaning.definitions);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
+            
               {" "}
               <strong>Definition: </strong>
               {definition.definition}
               <br />
              
               <em><Examples examples={definition.example}/></em>
-            </p>
+           
           
             <Synonyms synonyms={definition.synonyms} />
           </div>
