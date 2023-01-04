cec17afb4af7802c5e3680b3237e09fdc9e00a77
StefanieBaeuchler <stefanie.baeuchler@hotmail.com>
2023-01-04 10:27:13 +0100

    Integrated Pexels Api for picture search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,55 +1,68 @@
 import React, { useState } from "react";
 import Results from "./Results";
+import Photos from "./Photos";
 import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("sunset");
   const [results, setResults] = useState(null);
-  const [loaded, setLoaded]= useState (false);
+  const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
-  function handleResponse (response){
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
-
   }
 
-function search (){
-let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
 
-axios.get(apiUrl).then(handleResponse);
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000104cdab0573104fc2b6ffc45fd62fd377";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    search();  
+    search();
   }
 
   function updateKeyword(event) {
     setKeyword(event.target.value);
   }
 
-function load(){
-  setLoaded(true);
-  search();
-}
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
-  if (loaded){
-  return (
-    <div className="Dictionary">
-      <div className="firstSection">
-        <h2>📚 Dictionary</h2>
-        <h3>What word do you want to look up?</h3>
-        <form onSubmit={handleSubmit}>
-          <input type="search" onChange={updateKeyword} />
-        </form>
-        <div className="Hints">suggested words: sunset, house, food...</div>
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <div className="firstSection">
+          <h2>📚 Dictionary</h2>
+          <h3>What word do you want to look up?</h3>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={updateKeyword} />
+          </form>
+          <div className="Hints">suggested words: sunset, house, food...</div>
+        </div>
+
+        <Results results={results} />
+        <Photos photos={photos} />
       </div>
-
-      <Results results={results} />
-    </div>
-  );
-}else{
-  load();
-  return "Loading";
-}
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
